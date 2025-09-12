a79f9de7f00b7f55f4bbe93dfd87221cca94af10
Sand-Port <sahanddarwish05@gmail.com>
2025-09-11 21:56:20 -0500

    Update hello_fprintf.c

--- studio-1-hello-world/hello_fprintf.c
+++ studio-1-hello-world/hello_fprintf.c
@@ -1,1 +1,10 @@
-//Replace this file with the contents of your hello_fprintf.c
+// Sahand Darwish
+// 2025-09-11
+// This program prints a message to the console using fprintf()
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    fprintf(stdout, "Hello World!.\n");
+    return 0;
+}
