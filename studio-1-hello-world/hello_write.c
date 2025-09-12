cbd978f61cdeb938676379a0a05915293cbee9d4
Sand-Port <sahanddarwish05@gmail.com>
2025-09-11 21:58:07 -0500

    Update hello_write.c

--- studio-1-hello-world/hello_write.c
+++ studio-1-hello-world/hello_write.c
@@ -1,1 +1,11 @@
-//Replace the contents of this file with your hello_write.c
+// Sahand Darwish
+// 2025-09-11
+// This program prints a message to the console using the write() system call
+
+#include <unistd.h>  // for write() and STDOUT_FILENO
+
+int main(int argc, char* argv[]) {
+    const char message[] = "Hello from write()!\n";
+    write(STDOUT_FILENO, message, 200);  // 20 characters including the '\n'
+    return 0;
+}
