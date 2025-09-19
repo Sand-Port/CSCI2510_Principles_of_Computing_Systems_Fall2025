8fb810530e771c1f8ad41e559d96b8321d8300b3
Sand-port <sdarwish@h5.mcs.slu.edu>
2025-09-18 23:18:35 -0500

    hw2

--- studio-2-input-output/copy.c
+++ studio-2-input-output/copy.c
@@ -1,1 +1,22 @@
-//Replace this file with your copy.c
+#include <unistd.h>   // for read(), write(), STDIN_FILENO, STDOUT_FILENO
+
+#define bufferSize 200
+
+char buffer[bufferSize];
+
+int main(void) {
+    while (1) {
+        ssize_t bytesRead = read(STDIN_FILENO, buffer, bufferSize);
+
+        if (bytesRead == 0) {
+            // End of file reached
+            break;
+        }
+
+        // Write only the number of bytes actually read
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
+
+    return 0;
+}
+
